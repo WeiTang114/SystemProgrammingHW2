b5cb5f07f7e53ff3a3cc50be7273a7259262fc41
WeiTang114 <weitang114@gmail.com>
2014-11-15 00:54:17 +0800

    Deal cards

--- judge.c
+++ judge.c
@@ -6,6 +6,7 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include <time.h>
+#include <fcntl.h>
 
 #define STDIN 0
 #define STDOUT 1
@@ -28,6 +29,7 @@ typedef struct {
 	int card_num;
 	int rand_key;
 	int pid;
+	int fifo_w;
 } Player;
 
 
@@ -119,6 +121,7 @@ void init_players(char* judge_id, Player* players, int* player_ids)
 		p->idx = PL_IDXES[i];
 		p->card_num = 0;
 		p->rand_key = rand_keys[i];
+		p->fifo_w = 0;
 
 		pid = fork();
 		if (pid < 0) {
@@ -145,6 +148,91 @@ void init_players(char* judge_id, Player* players, int* player_ids)
 }
 
 
+
+void shuffle(int *array, size_t n)
+{
+    if (n > 1) 
+    {
+        size_t i;
+        for (i = 0; i < n - 1; i++) 
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = array[j];
+          array[j] = array[i];
+          array[i] = t;
+        }
+    }
+}
+
+void deal_cards(Player* players)
+{
+	int indexarr[53];
+	int cards_tmp[53];
+	int cards[53];
+	char cards_buf[4][64] = {{}, {}, {}, {}};
+	int index[4][2] = {{0, 13}, {14, 26}, {27, 39}, {40, 52}};
+
+	DP("deal\n");
+
+	// initialize cards
+	for (int i = 0; i < 53; i++) {
+		indexarr[i] = i;
+		cards_tmp[i] = (i / 4) + 1;
+	}	
+	cards_tmp[52] = 0;
+
+	// shuffle the card indexes
+	shuffle(indexarr, 53);
+	for (int j = 0; j < 53; j++) {
+		cards[j] = cards_tmp[indexarr[j]];
+	}
+
+	// put cards to strings
+	for (int k = 0; k < 4; k++) {
+		memset(cards_buf[k], 0, sizeof(cards_buf[k]));
+		for (int cd = index[k][0]; cd <= index[k][1]; cd++) {
+			sprintf(cards_buf[k], "%s%d ", cards_buf[k], cards[cd]);
+		}
+		cards_buf[k][strlen(cards_buf[k]) - 1] = '\0';
+	}
+
+	// send
+	for (int m = 0; m < 4; m++) {
+		write(players[m].fifo_w, cards_buf[m], sizeof(cards_buf[m]));
+	}
+}
+
+/**
+ * [run_game description]
+ * @param  judge_id
+ * @param  players
+ * @return the loser's id (the "id" field in Player)
+ */
+int run_game(char* judge_id, Player* players)
+{
+	int loser_id = players[0].id;
+
+	int fifo_j = 0;
+
+	char fifojudge[32] = {};
+	char fifoplayer[32] = {};
+
+	sprintf(fifojudge, "./judge%s.FIFO", judge_id);
+	DP("aaa\n");
+	fifo_j = open(fifojudge, O_RDONLY);
+	DP("bbb\n");
+	for (int i = 0; i < 4; i++) {
+		memset(fifoplayer, 0, sizeof(fifoplayer));
+		sprintf(fifoplayer, "./judge%s_%c.FIFO", judge_id, PL_IDXES[i]);
+		players[i].fifo_w = open(fifoplayer, O_WRONLY);
+	}
+
+	deal_cards(players);
+
+
+	return loser_id;
+}
+
 int main(int argc, char** argv)
 {
 	if (argc < 2) {
@@ -157,6 +245,8 @@ int main(int argc, char** argv)
 	char buf[512] = {};
 	int player_ids[4] = {};
 	Player players[4];
+	int loser_id = -1;
+
 	sprintf(judge_id, "%s", argv[1]);
 
 	char judge_yo[32];
@@ -183,9 +273,10 @@ int main(int argc, char** argv)
 		create_fifos(judge_id);
 		init_players(judge_id, players, player_ids);
 
-		char* token = strtok(buf, " ");
-		DP("token = \"%s\"\n", token);
-		printf("%s", token);
+		loser_id = run_game(judge_id, players);
+
+		DP("loser = %d\n", loser_id);
+		printf("%d", loser_id);
 		fflush(stdout);
 	}
 
--- player.c
+++ player.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 int main(int argv, char** argc)
 {
@@ -11,5 +12,16 @@ int main(int argv, char** argc)
 
 	printf("Player %s created.\n", argc[2]);
 	fflush(stdout);
+
+	char fifo_r_path[64];
+	char fifo_w_path[64];
+	char buf[64];
+	sprintf(fifo_w_path, "./judge%s.FIFO", argc[1]);
+	sprintf(fifo_r_path, "./judge%s_%s.FIFO", argc[1], argc[2]);
+	int fifo_w = open(fifo_w_path, O_WRONLY);
+	int fifo_r = open(fifo_r_path, O_RDONLY);
+	read(fifo_r, buf, sizeof(buf));
+
+	printf("Player %s get \"%s\"\n", argc[2], buf);
 
 }
